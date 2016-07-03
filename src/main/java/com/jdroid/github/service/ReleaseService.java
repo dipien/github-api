c7d576c0812b3b25bdddce837ad897f9447919cc
Maxi Rosson <maxirosson@gmail.com>
2016-07-03 12:00:57 -0300

    Add getReleaseByTagName support. Close #3

--- src/main/java/com/jdroid/github/service/ReleaseService.java
+++ src/main/java/com/jdroid/github/service/ReleaseService.java
@@ -3,11 +3,13 @@ package com.jdroid.github.service;
 import com.jdroid.github.IRepositoryIdProvider;
 import com.jdroid.github.Release;
 import com.jdroid.github.client.GitHubClient;
+import com.jdroid.github.client.GitHubRequest;
 
 import java.io.IOException;
 
 import static com.jdroid.github.client.IGitHubConstants.SEGMENT_RELEASES;
 import static com.jdroid.github.client.IGitHubConstants.SEGMENT_REPOS;
+import static com.jdroid.github.client.IGitHubConstants.SEGMENT_TAGS;
 
 public class ReleaseService extends GitHubService {
 
@@ -26,5 +28,24 @@ public class ReleaseService extends GitHubService {
 		uri.append('/').append(repoId);
 		uri.append(SEGMENT_RELEASES);
 		return client.post(uri.toString(), release, Release.class);
+	}
+
+	public Release getReleaseByTagName(IRepositoryIdProvider repositoryIdProvider, String tagName) throws IOException {
+		String repoId = getId(repositoryIdProvider);
+
+		if (tagName == null) {
+			throw new IllegalArgumentException("Tag name cannot be null");
+		}
+
+		StringBuilder uri = new StringBuilder(SEGMENT_REPOS);
+		uri.append('/').append(repoId);
+		uri.append(SEGMENT_RELEASES);
+		uri.append(SEGMENT_TAGS);
+		uri.append('/').append(tagName);
+
+		GitHubRequest request = createRequest();
+		request.setUri(uri);
+		request.setType(Release.class);
+		return (Release) client.get(request).getBody();
 	}
 }
