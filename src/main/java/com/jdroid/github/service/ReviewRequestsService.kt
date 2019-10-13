8ebb56a96afaeb90c782abc5424e2713b4b9dd46
Maxi Rosson <maxirosson@gmail.com>
2019-10-13 15:35:51 -0300

    Ktlint fixes

--- src/main/java/com/jdroid/github/service/ReviewRequestsService.kt
+++ src/main/java/com/jdroid/github/service/ReviewRequestsService.kt
@@ -5,22 +5,22 @@ import com.jdroid.github.client.GitHubClient
 import com.jdroid.github.client.IGitHubConstants
 
 // https://developer.github.com/v3/pulls/review_requests/
-class ReviewRequestsService: GitHubService {
+class ReviewRequestsService : GitHubService {
 
-	constructor() : super()
+    constructor() : super()
 
-	constructor(client: GitHubClient) : super(client)
+    constructor(client: GitHubClient) : super(client)
 
-	fun createReviewRequest(repository: IRepositoryIdProvider, pullNumber: Int, reviewers: List<String>? = emptyList(), teamReviewers: List<String>? = emptyList()) {
-		val repoId = getId(repository)
-		val uri = StringBuilder(IGitHubConstants.SEGMENT_REPOS)
-		uri.append("/").append(repoId)
-		uri.append(IGitHubConstants.SEGMENT_PULLS)
-		uri.append("/").append(pullNumber)
-		uri.append("/requested_reviewers")
-		val params = mutableMapOf<String, Any>()
-		params["reviewers"] = reviewers.orEmpty()
-		params["team_reviewers"] = teamReviewers.orEmpty()
-		client.post<Any>(uri.toString(), params, null)
-	}
+    fun createReviewRequest(repository: IRepositoryIdProvider, pullNumber: Int, reviewers: List<String>? = emptyList(), teamReviewers: List<String>? = emptyList()) {
+        val repoId = getId(repository)
+        val uri = StringBuilder(IGitHubConstants.SEGMENT_REPOS)
+        uri.append("/").append(repoId)
+        uri.append(IGitHubConstants.SEGMENT_PULLS)
+        uri.append("/").append(pullNumber)
+        uri.append("/requested_reviewers")
+        val params = mutableMapOf<String, Any>()
+        params["reviewers"] = reviewers.orEmpty()
+        params["team_reviewers"] = teamReviewers.orEmpty()
+        client.post<Any>(uri.toString(), params, null)
+    }
 }
