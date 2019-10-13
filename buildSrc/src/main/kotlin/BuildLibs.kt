b480933dbeae5cc08719f1e546c405846e4bb0f8
Maxi Rosson <maxirosson@gmail.com>
2019-10-13 10:27:53 -0300

    Upgraded config and dependencies

---
+++ buildSrc/src/main/kotlin/BuildLibs.kt
@@ -0,0 +1,7 @@
+object BuildLibs {
+    // https://github.com/maxirosson/jdroid-gradle-plugin/releases
+    const val JDROID_GRADLE_JAVA_PLUGIN = "com.jdroidtools:jdroid-gradle-java-plugin:2.4.0-SNAPSHOT"
+
+    // https://github.com/maxirosson/jdroid-component-builder/blob/master/CHANGELOG.md
+    const val JDROID_COMPONENT_BUILDER = "com.jdroidtools:jdroid-component-builder:2.2.0"
+}
