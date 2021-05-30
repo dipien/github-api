4a64a65aead0e96a4b343b50f0bb4f4fa4de6edf
jdroid-ci <jdroidtools@gmail.com>
2021-05-30 00:08:28 +0000

    Upgraded com.google.code.gson:gson from 2.8.6 to 2.8.7

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -1,4 +1,4 @@
 object Libs {
     // https://github.com/google/gson/blob/master/CHANGELOG.md
-    const val GSON = "com.google.code.gson:gson:2.8.6"
+    const val GSON = "com.google.code.gson:gson:2.8.7"
 }
