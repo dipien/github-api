2ef3f954d3e5967893811131e7961613026e2cd4
Maxi Rosson <maxirosson@gmail.com>
2021-08-10 09:40:49 -0300

    Migrate to Dipien

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
-	id("com.gradle.enterprise").version("3.3.4")
+	id("com.gradle.enterprise").version("3.6.3")
 }
 
 include(":jdroid-java-github")
+
+apply(from = java.io.File(settingsDir, "buildCacheSettings.gradle"))
